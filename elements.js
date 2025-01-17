41d748eef428ecef1e271381f67715888998695c
Kizzyan <yan.leitao26@gmail.com>
2025-01-17 11:13:02 -0300

    Fix: Correção no arquivo elements.js
    Tinha esquecio de escapar as aspas internas da string

--- elements.js
+++ elements.js
@@ -1,31 +1,31 @@
 elements = {
-    "WAJAJA": {
-        "desc": "WAJAJA é a entidade da consciência. Ter a própria percepção que o Meiaum se fodeu de novo agrada o elemento de WAJAJA.",
-        "fraseGuia": "",
-        "outrasInfo": {}
-    },
+  "WAJAJA": {
+    "desc": "WAJAJA é a entidade da consciência. Ter a própria percepção que o Meiaum se fodeu de novo agrada o elemento de WAJAJA.",
+    "fraseGuia": "",
+    "outrasInfo": {}
+  },
 
-    "ehehe": {
-        "desc": "ehehe é a entidade do caos. Tudo que envolve picuinha e a transformação da live agrada a entidade de ehehe.",
-        "fraseGuia": "",
-        "outrasInfo": {}
-    },
+  "ehehe": {
+    "desc": "ehehe é a entidade do caos. Tudo que envolve picuinha e a transformação da live agrada a entidade de ehehe.",
+    "fraseGuia": "",
+    "outrasInfo": {}
+  },
 
-    "PixEscorregadio": {
-        "desc": "PixEscorregadio é a entidade do tempo. Ela busca os momentos vivenciados, distorcendo a percepção do timer do indivíduo para seu próprio agrado.",
-        "fraseGuia": "",
-        "outrasInfo": {}
-    },
+  "PixEscorregadio": {
+    "desc": "PixEscorregadio é a entidade do tempo. Ela busca os momentos vivenciados, distorcendo a percepção do timer do indivíduo para seu próprio agrado.",
+    "fraseGuia": "",
+    "outrasInfo": {}
+  },
 
-    "BaileInsano": {
-        "desc": "BaileInsano é a entidade do sentimento. Ela busca a intensidade: Meiaum música, zueira no sync, vídeos insalubres - tudo que envolve sentir uma emoção extrema agrada a entidade de BaileInsano.",
-        "fraseGuia": "",
-        "outrasInfo": {}
-    },
+  "BaileInsano": {
+    "desc": "BaileInsano é a entidade do sentimento. Ela busca a intensidade: Meiaum música, zueira no sync, vídeos insalubres - tudo que envolve sentir uma emoção extrema agrada a entidade de BaileInsano.",
+    "fraseGuia": "",
+    "outrasInfo": {}
+  },
 
-    "pixjumpscare": {
-        "desc": "O pixjumpscare é a entidade da própria subathon e o elemento mais misterioso. Os poucos que presenciaram tamanha manifestação, também descreveram ter visto "um vislumbre de Deus", como se o próprio cérebro humano não pudesse processar um evento impossível como este. Não se sabe nada sobre pessoas com afinidade a esse elemento.",
-        "fraseGuia": "",
-        "outrasInfo": {}
-    }
+  "pixjumpscare": {
+    "desc": "O pixjumpscare é a entidade da própria subathon e o elemento mais misterioso. Os poucos que presenciaram tamanha manifestação, também descreveram ter visto \"um vislumbre de Deus\", como se o próprio cérebro humano não pudesse processar um evento impossível como este. Não se sabe nada sobre pessoas com afinidade a esse elemento.",
+    "fraseGuia": "",
+    "outrasInfo": {}
+  }
 }
