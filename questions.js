bcced55919bf41277ffff9d7f3f5a390d504439c
Kizzyan <yan.leitao26@gmail.com>
2025-01-17 12:56:31 -0300

    Chore: Mudança de cores e elementos animados

--- questions.js
+++ questions.js
@@ -1,202 +1,202 @@
 questions = [
-    {
-        "id": 1,
-        "question": "Você permanece calmo e concentrado mesmo em contas engraçadas",
-        "effect": {
-            "emocao": -1,
-            "solidao": 1,
-            "ordem": 2,
-            "caos": -2,
-        }
-    },
-    {
-        "id": 2,
-        "question": "Você raramente reflete as consequências de suas mensagens.",
-        "effect": {
-            "emocao": 1,
-            "solidao": -1,
-            "ordem": -2,
-            "caos": 2,
-        }
-    },
-    {
-        "id": 3,
-        "question": "Suas emoções mandam em você mais do que você manda nelas.",
-        "effect": {
-            "emocao": 2,
-            "solidao": -2,
-            "ordem": -1,
-            "caos": 1,
-        }
-    },
-    {
-        "id": 4,
-        "question": "Você baseia seus pixes por seus impactos no presente, não no futuro.",
-        "effect": {
-            "emocao": -2,
-            "solidao": 2,
-            "ordem": 1,
-            "caos": -1, 
-        }
-    },
-    {
-        "id": 5,
-        "question": "Você prefere trabalhar em um time onde todos doem 1 real.",
-        "effect": {
-            "emocao": -1,
-            "solidao": 1,
-            "ordem": 2,
-            "caos": -2,
-        }
-    },
-    {
-        "id": 6,
-        "question": "Você tende a respeitar o Meiaum",
-        "effect": {
-            "emocao": -1,
-            "solidao": 1,
-            "ordem": 2,
-            "caos": -2,
-        }
-    },
-    {
-        "id": 7,
-        "question": "Você frequentemente fica preso no pixie.gg.",
-        "effect": {
-            "emocao": -2,
-            "solidao": 2,
-            "ordem": 1,
-            "caos": -1,
-        }
-    },
-    {
-        "id": 8,
-        "question": "Velocidade em mandar emotes é uma das qualidades mais admiráveis em um indivíduo.",
-        "effect": {
-            "emocao": 2,
-            "solidao": -2,
-            "ordem": -1,
-            "caos": 1,
-        }
-    },
-    {
-        "id": 9,
-        "question": "Você enxerga o timer como uma referência, não uma obrigação.",
-        "effect": {
-            "emocao": 1,
-            "solidao": -1,
-            "ordem": -2,
-            "caos": 2,
-        }
-    },
-    {
-        "id": 10,
-        "question": "Você geralmente faz a conta de quantos dias tem no timer mesmo que ela machuque alguém.",
-        "effect": {
-            "emocao": -1,
-            "solidao": 1,
-            "ordem": 2,
-            "caos": -2,
-        }
-    },
-    {
-        "id": 11,
-        "question": "Você seguiria uma streak de música no sync mesmo se ela ferisse os outros.",
-        "effect": {
-            "emocao": 2,
-            "solidao": -2,
-            "ordem": -1,
-            "caos": 1,
-        }
-    },
-    {
-        "id": 12,
-        "question": "Planejamento não é a parte mais importante de um evento da live.",
-        "effect": {
-            "emocao": 1,
-            "solidao": -1,
-            "ordem": -2,
-            "caos": 2,
-        }
-    },
-    {
-        "id": 13,
-        "question": "Não existem inocentes na live, todos devem ver conteúdo insalubre igualmente.",
-        "effect": {
-            "emocao": 2,
-            "solidao": -2,
-            "ordem": -1,
-            "caos": 1,
-        }
-    },
-    {
-        "id": 14,
-        "question": "Pessoas que são mais lentas que você te estressam.",
-        "effect": {
-            "emocao": -2,
-            "solidao": 2,
-            "ordem": 1,
-            "caos": -1, 
-        }
-    },
-    {
-        "id": 15,
-        "question": "Você tem dificuldade em sentir empatia ao mandar conteúdo insalubre.",
-        "effect": {
-            "emocao": 2,
-            "solidao": -2,
-            "ordem": -1,
-            "caos": 1,
-        }
-    },
-    {
-        "id": 16,
-        "question": "Você acredita que não deve explicar suas mensagens se você escreveu corretamente.",
-        "effect": {
-            "emocao": -1,
-            "solidao": 1,
-            "ordem": 2,
-            "caos": -2,
-        }
-    },
-    {
-        "id": 17,
-        "question": "Você tem dificuldade em dar pix em grupo.",
-        "effect": {
-            "emocao": -2,
-            "solidao": 2,
-            "ordem": 1,
-            "caos": -1,
-        }
-    },
-    {
-        "id": 18,
-        "question": "Seus pixes tendem a ser práticos.",
-        "effect": {
-            "emocao": -2,
-            "solidao": 2,
-            "ordem": 1,
-            "caos": -1,
-        }
-    },
-    {
-        "id": 19,
-        "question": "Se o resultado for positivo, pouco importa a qualidade da produção.",
-        "effect": {
-            "emocao": 1,
-            "solidao": -1,
-            "ordem": -2,
-            "caos": 2,
-        }
-    },
-    {
-        "id": 20,
-        "question": "Você não se esforça em planejar mensagens.",
-        "effect": {
-            "emocao": 1,
-            "solidao": -1,
-            "ordem": -2,
-            "caos": 2,
-        }
-    },
+  {
+    "id": 1,
+    "question": "Você permanece calmo e concentrado mesmo em contas engraçadas",
+    "effect": {
+      "emocao": -1,
+      "solidao": 1,
+      "ordem": 2,
+      "caos": -2,
+    }
+  },
+  {
+    "id": 2,
+    "question": "Você raramente reflete as consequências de suas mensagens.",
+    "effect": {
+      "emocao": 1,
+      "solidao": -1,
+      "ordem": -2,
+      "caos": 2,
+    }
+  },
+  {
+    "id": 3,
+    "question": "Suas emoções mandam em você mais do que você manda nelas.",
+    "effect": {
+      "emocao": 2,
+      "solidao": -2,
+      "ordem": -1,
+      "caos": 1,
+    }
+  },
+  {
+    "id": 4,
+    "question": "Você baseia seus pixes por seus impactos no presente, não no futuro.",
+    "effect": {
+      "emocao": -2,
+      "solidao": 2,
+      "ordem": 1,
+      "caos": -1,
+    }
+  },
+  {
+    "id": 5,
+    "question": "Você prefere trabalhar em um time onde todos doem 1 real.",
+    "effect": {
+      "emocao": -1,
+      "solidao": 1,
+      "ordem": 2,
+      "caos": -2,
+    }
+  },
+  {
+    "id": 6,
+    "question": "Você tende a não respeitar o Meiaum",
+    "effect": {
+      "emocao": -1,
+      "solidao": 1,
+      "ordem": 2,
+      "caos": -2,
+    }
+  },
+  {
+    "id": 7,
+    "question": "Você frequentemente fica preso no pixie.gg.",
+    "effect": {
+      "emocao": -2,
+      "solidao": 2,
+      "ordem": 1,
+      "caos": -1,
+    }
+  },
+  {
+    "id": 8,
+    "question": "Velocidade em mandar emotes é uma das qualidades mais admiráveis em um indivíduo.",
+    "effect": {
+      "emocao": 2,
+      "solidao": -2,
+      "ordem": -1,
+      "caos": 1,
+    }
+  },
+  {
+    "id": 9,
+    "question": "Você enxerga o timer como uma referência, não uma obrigação.",
+    "effect": {
+      "emocao": 1,
+      "solidao": -1,
+      "ordem": -2,
+      "caos": 2,
+    }
+  },
+  {
+    "id": 10,
+    "question": "Você geralmente faz a conta de quantos dias tem no timer mesmo que ela machuque alguém.",
+    "effect": {
+      "emocao": -1,
+      "solidao": 1,
+      "ordem": 2,
+      "caos": -2,
+    }
+  },
+  {
+    "id": 11,
+    "question": "Você seguiria uma streak de música no sync mesmo se ela ferisse os outros.",
+    "effect": {
+      "emocao": 2,
+      "solidao": -2,
+      "ordem": -1,
+      "caos": 1,
+    }
+  },
+  {
+    "id": 12,
+    "question": "Planejamento não é a parte mais importante de um evento da live.",
+    "effect": {
+      "emocao": 1,
+      "solidao": -1,
+      "ordem": -2,
+      "caos": 2,
+    }
+  },
+  {
+    "id": 13,
+    "question": "Não existem inocentes na live, todos devem ver conteúdo insalubre igualmente.",
+    "effect": {
+      "emocao": 2,
+      "solidao": -2,
+      "ordem": -1,
+      "caos": 1,
+    }
+  },
+  {
+    "id": 14,
+    "question": "Pessoas que são mais lentas que você te estressam.",
+    "effect": {
+      "emocao": -2,
+      "solidao": 2,
+      "ordem": 1,
+      "caos": -1,
+    }
+  },
+  {
+    "id": 15,
+    "question": "Você tem dificuldade em sentir empatia ao mandar conteúdo insalubre.",
+    "effect": {
+      "emocao": 2,
+      "solidao": -2,
+      "ordem": -1,
+      "caos": 1,
+    }
+  },
+  {
+    "id": 16,
+    "question": "Você acredita que não deve explicar suas mensagens se você escreveu corretamente.",
+    "effect": {
+      "emocao": -1,
+      "solidao": 1,
+      "ordem": 2,
+      "caos": -2,
+    }
+  },
+  {
+    "id": 17,
+    "question": "Você tem dificuldade em dar pix em grupo.",
+    "effect": {
+      "emocao": -2,
+      "solidao": 2,
+      "ordem": 1,
+      "caos": -1,
+    }
+  },
+  {
+    "id": 18,
+    "question": "Seus pixes tendem a ser práticos.",
+    "effect": {
+      "emocao": -2,
+      "solidao": 2,
+      "ordem": 1,
+      "caos": -1,
+    }
+  },
+  {
+    "id": 19,
+    "question": "Se o resultado for positivo, pouco importa a qualidade da produção.",
+    "effect": {
+      "emocao": 1,
+      "solidao": -1,
+      "ordem": -2,
+      "caos": 2,
+    }
+  },
+  {
+    "id": 20,
+    "question": "Você não se esforça em planejar mensagens.",
+    "effect": {
+      "emocao": 1,
+      "solidao": -1,
+      "ordem": -2,
+      "caos": 2,
+    }
+  },
 ]
